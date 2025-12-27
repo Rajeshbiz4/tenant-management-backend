557d5dfe4a912e253a6780e66e3b699ba2b658cb
rajeshbiz4 <rajesh.pandhare181@gmail.com>
2025-12-27 08:23:52 +0530

    property page modal changes

--- models/Property.js
+++ models/Property.js
@@ -1,52 +1,97 @@
 const mongoose = require('mongoose');
 
-const propertySchema = new mongoose.Schema({
-  propertyType: {
-    type: String,
-    required: true,
-    enum: ['flat', 'shop', 'plot']
+const agreementSchema = new mongoose.Schema({
+  startDate: {
+    type: Date,
+    required: true
   },
-  area: {
+  months: {
     type: Number,
     required: true,
-    min: 0
-  },
-  location: {
-    type: String,
-    required: true,
-    trim: true
-  },
+    min: 1
+  }
+}, { _id: false });
+
+const rentSchema = new mongoose.Schema({
   monthlyRent: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-  shopName: {
-    type: String,
-    required: true,
-     trim: true
-  },
-  shopNumber: {
     type: Number,
     required: true,
     min: 0
   },
   maintenance: {
     type: Number,
-    required: true,
-    min: 0,
-    default: 0
+    default: 0,
+    min: 0
   },
-  lightBill: {
+  lastPaid: {
+    type: Date
+  }
+}, { _id: false });
+
+const electricitySchema = new mongoose.Schema({
+  submeterNo: {
+    type: String,
+    trim: true
+  },
+  lastUnit: {
     type: Number,
-    min: 0,
-    default: 0
+    default: 0,
+    min: 0
   },
+  unitRate: {
+    type: Number,
+    default: 0,
+    min: 0
+  }
+}, { _id: false });
+
+const propertySchema = new mongoose.Schema({
+  propertyType: {
+    type: String,
+    required: true,
+    enum: ['flat', 'shop', 'plot']
+  },
+
+  shopName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  shopNumber: {
+    type: String,
+    required: true
+  },
+
+  area: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+
+  location: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  agreement: agreementSchema,
+
+  rent: rentSchema,
+
+  electricity: electricitySchema,
+
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
   },
+
   tenant: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Tenant',
@@ -57,4 +102,3 @@ const propertySchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Property', propertySchema);
-
